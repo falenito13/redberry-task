053b29065d815aa9a11c504afc8368e55244c207
david <david.l@app3null.com>
2022-02-13 19:46:11 +0400

    validation errors in login page

--- resources/js/components/Pages/Login.jsx
+++ resources/js/components/Pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
     const {lang} = useParams();
     const {t, i18n} = useTranslation();
     const {register, handleSubmit} = useForm();
-    const [error,setError] = useState();
+    const [errors,setErrors] = useState([]);
     let navigate = useNavigate();
     let location = useLocation();
     const authenticatedCallback = () => {
@@ -18,17 +18,11 @@ export default function Login() {
 
     const onSubmit = (data) => {
         axios.post('/api/login', data).then(response => {
-           if(response.data.email){
-               setError(response.data.email[0])
-           }
-           if(response.data.password){
-               setError(response.data.password[0])
-           }
-           if(response.data.auth){
-               setError(response.data.auth[0])
-           }
-            if (response.data.token) {
+            if(response.data.status === 200){
                 User.authenticated(response.data, authenticatedCallback)
+            }
+            else {
+                setErrors(response.data)
             }
         })
     }
@@ -45,6 +39,7 @@ export default function Login() {
                         className="border rounded py-3 w-full sm:pr-22 px-2 placeholder-gray-light placeholder:py-4.5 rounded-lg"
                         type={'email'}
                         placeholder={t('email')} {...register('email')} />
+                    {errors.email && <p className={'text-custom-red text-md mt-1'}>{errors.email}</p>}
                 </div>
                 <div className={'mt-10'}>
                     <label className="block text-black-dark text-sm font-bold mb-2" htmlFor="password">
@@ -54,10 +49,10 @@ export default function Login() {
                         className="border rounded py-3 w-full sm:pr-22 px-2 placeholder-gray-light placeholder:py-4.5 rounded-lg"
                         type={'password'}
                         placeholder={t('password')} {...register('password')} />
+                    {errors.password && <p className={'text-custom-red text-md mt-1'}>{errors.password}</p> }
                 </div>
-                {error && (
-                    <p className={'text-custom-red text-md text-center mt-2'}>{error}</p>
-                )}
+                {errors.auth && <p className={'text-custom-red text-md mt-1 text-center'}>{errors.auth}</p> }
+
                 <div className={'mt-6'}>
                     <button type={'submit'}
                             className={'border px-32 sm:px-40 py-4.5 rounded-4xl px-2 text-uppercase text-sm text-white bg-pink-dark'}>
