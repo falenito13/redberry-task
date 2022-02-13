f4b9c4c9f0f4cc40d18f4907ab0027006bc4cc25
falenito13 <56035351+falenito13@users.noreply.github.com>
2022-02-13 20:36:01 +0400

    remove testing route from web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test',[\App\Http\Controllers\StatisticController::class,'summaryStatisticData']);
 Route::get('/{any?}', function () {
     return view('app');
 })->where('any', '^.*$');
